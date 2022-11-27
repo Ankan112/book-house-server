b65323aecfd58fddfedb41628452cf7c863aeaba
Ankan112 <ankanhalder112@gmail.com>
2022-11-27 23:55:24 +0600

    buyer delete method

--- index.js
+++ index.js
@@ -101,6 +101,12 @@ async function run() {
             const result = await userCollection.deleteOne(query)
             res.send(result)
         })
+        app.delete('/users/buyer/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+        })
     }
     finally {
 
